eb1226be7fc2a06d6230f32c94e946162217568b
Richter Rojas <richter9721@outlook.com>
2025-09-15 22:14:40 -0500

    Proyecto corregido

--- database/scripts/1. Borrar agencias.sql
+++
@@ -1,9 +0,0 @@
--- Desactivar verificación de llaves foráneas
-SET FOREIGN_KEY_CHECKS = 0;
-
--- Borrar filas de la tabla (ejemplo con condición)
-DELETE FROM agencias
-WHERE id_agencia in (1,4,6);
-
--- Reactivar verificación de llaves foráneas
-SET FOREIGN_KEY_CHECKS = 1;
--- database/scripts/2.1. Eliminar tablas.sql
+++
@@ -1,9 +0,0 @@
-
--- Desactivar verificación de llaves foráneas
-SET FOREIGN_KEY_CHECKS = 0;
-
-
-DROP TABLE solucion_master.dispositivos;
-
--- Reactivar verificación de llaves foráneas
-SET FOREIGN_KEY_CHECKS = 1;
