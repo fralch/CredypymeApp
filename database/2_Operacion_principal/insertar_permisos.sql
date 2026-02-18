3d241b02f92b539999a3048bb71eb178c9fb02fb
Richter Rojas <richter9721@outlook.com>
2026-02-17 21:52:42 -0500

    Avance: Ubicación nuevos módulos

---
+++ database/2_Operacion_principal/insertar_permisos.sql
@@ -0,0 +1,5 @@
+insert into permisos (area,modulo) values ('CREDITOS_GRUPAL','PROPUESTA');
+insert into permisos (area,modulo) values ('CREDITOS_GRUPAL','COPIA_PROPUESTA');
+insert into permisos (area,modulo) values ('CREDITOS_GRUPAL','APROBACION');
+insert into permisos (area,modulo) values ('CREDITOS_GRUPAL','COPIA_APROBACION');
+insert into permisos (area,modulo) values ('CREDITOS_GRUPAL','DOCUMENTOS_FINANCIEROS');
